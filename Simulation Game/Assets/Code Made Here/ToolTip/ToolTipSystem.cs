a9812a03ae7fae91948d3aad10040df3b8affe76
Julian Lanty <julianemanueltejo@gmail.com>
2023-01-09 19:52:33 -0300

    some ui and bugfixes

--- Simulation Game/Assets/Code Made Here/ToolTip/ToolTipSystem.cs
+++ Simulation Game/Assets/Code Made Here/ToolTip/ToolTipSystem.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class ToolTipSystem : MonoBehaviour
 {
-    public GameObject ToolTip;
+    public GameObject ToolTipBG;
+    public GameObject ToolTipText;
+    
     public ToolTipScreenSpaceUI ToolTipScreenSpaceUI;
     private static ToolTipSystem current;
 
@@ -21,12 +23,17 @@ public class ToolTipSystem : MonoBehaviour
 
     public static void Show(string content)
     {
-        current.ToolTip.SetActive(true);
+        current.ToolTipBG.SetActive(true);
+        current.ToolTipText.SetActive(true);
         current.ToolTipScreenSpaceUI.SetText(content);
     }
     public static void Hide()
     {
-        if (current.ToolTip) current.ToolTip.SetActive(false);
+        if (current.ToolTipBG)
+        {
+            current.ToolTipBG.SetActive(false);
+            current.ToolTipText.SetActive(false);
+        }
     }
     
 }
